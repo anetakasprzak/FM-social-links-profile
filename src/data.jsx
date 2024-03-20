92f3155525802a6e9461649c650e5cab9c100674
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-03-20 14:40:48 +0000

    creating data

---
+++ src/data.jsx
@@ -0,0 +1,15 @@
+export const userData = {
+  image: "./assets/avatar-jessica.jpeg",
+  name: "Jessica Randall",
+  city: "London",
+  country: "United Kingdom",
+  skills: "Front-end developer and avid reader",
+};
+
+export const socialLinks = [
+  { id: 1, name: "GitHub" },
+  { id: 2, name: "Frontend Mentor" },
+  { id: 3, name: "LinkedIn" },
+  { id: 4, name: "Twitter" },
+  { id: 5, name: "Instagram" },
+];
